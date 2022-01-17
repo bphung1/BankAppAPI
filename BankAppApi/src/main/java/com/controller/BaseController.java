90d96f3210ac1c0e392c7d6862ce1b2ce5b3e63d
Bao Phung <b.giaphung@gmail.com>
2022-01-17 12:40:15 -0600

    change cross orogin

--- BankAppApi/src/main/java/com/controller/BaseController.java
+++ BankAppApi/src/main/java/com/controller/BaseController.java
@@ -4,6 +4,6 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequestMapping("/api")
-@CrossOrigin(origins = "http://localhost:4200/")
+@CrossOrigin(origins = "http://localhost:3000/")
 public class BaseController {
 }
