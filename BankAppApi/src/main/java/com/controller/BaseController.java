506792b13b2db43ab8cbe64b2414f91d0c715f45
Bao Phung <b.giaphung@gmail.com>
2022-01-19 10:49:57 -0600

    remove cross origin annotation

--- BankAppApi/src/main/java/com/controller/BaseController.java
+++ BankAppApi/src/main/java/com/controller/BaseController.java
@@ -1,9 +1,7 @@
 package com.controller;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequestMapping("/api")
-@CrossOrigin(origins = "http://localhost:3000")
 public class BaseController {
 }
