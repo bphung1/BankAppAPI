bb8e866f686b35fb367a4d74c125ef4eca1fb55b
Bao Phung <b.giaphung@gmail.com>
2022-01-20 12:02:52 -0600

    add mac cross origin

--- BankAppApi/src/main/java/com/config/WebSecurityConfig.java
+++ BankAppApi/src/main/java/com/config/WebSecurityConfig.java
@@ -76,7 +76,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000")); //or add * to allow all origins
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000", "http://mac-mini:3000")); //or add * to allow all origins
         configuration.setAllowCredentials(true);
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")); //to set allowed http methods
         configuration.setAllowedHeaders(Arrays.asList("token", "Cache-Control", "Content-Type"));
